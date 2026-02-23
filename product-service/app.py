0a4a912760f145b4bc11b60f997dab7151d29339
kuruvauday <kuruvauday777.com>
2026-02-23 02:53:41 +0000

    product html

--- product-service/app.py
+++ product-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
@@ -7,6 +7,9 @@ products = [
     {"id": 2, "name": "Phone", "price": 500},
 ]
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 @app.route("/products", methods=["GET"])
 def get_products():
     return jsonify(products)
